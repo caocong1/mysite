5b7b69c2d7650c941c51e721882bc8aa4d6425ec
caocong <caocong1@gmail.com>
2019-07-14 16:18:42 +0800

    description author

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `CAOCONG的小站`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`
+    description: `xxx`,
+    author: `caocong`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
